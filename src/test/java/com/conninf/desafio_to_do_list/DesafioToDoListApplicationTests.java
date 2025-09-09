b187f6596a950c030886e061a220439a8d04c7b7
Michel Policeno <michel.policeno@hotmail.com>
2025-09-08 21:23:33 -0300

    Initial commit

---
+++ src/test/java/com/conninf/desafio_to_do_list/DesafioToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conninf.desafio_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
