30a67c0ee1b41704c6c042e0e1e116adbad819dd
Michel Policeno <michel.policeno@hotmail.com>
2025-09-08 21:50:11 -0300

    feat: repository created

---
+++ src/main/java/com/conninf/desafio_to_do_list/repository/ToDoRepository.java
@@ -0,0 +1,7 @@
+package com.conninf.desafio_to_do_list.repository;
+
+import com.conninf.desafio_to_do_list.entity.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToDoRepository extends JpaRepository<ToDo,Long> {
+}
